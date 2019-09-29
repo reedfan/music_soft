e5c8e2ef9e780961951a510c18e21f7d31b51436
reedfan <1617950759@qq.com>
2019-09-30 00:08:57 +0800

    code

---
+++ src/main/java/com/ustc/reed/pojo/TbSysAdmin.java
@@ -0,0 +1,11 @@
+package com.ustc.reed.pojo;
+
+import lombok.Data;
+
+@Data
+public class TbSysAdmin {
+    private Integer id;
+    private String userName;
+    private String passWord;
+    private String phoneNumber;
+}
